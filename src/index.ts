ccbc8e9f0a995b6cdb029777695fba23890bc816
PridyOK <aaron@alteredpixels.io>
2021-04-15 16:04:03 -0700

    Added Jest

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,4 @@ export const sum = (a: number, b: number): number => {
   return a + b
 }
 
-const testSum: number = sum(10, 15)
-
-console.log('test sum:', testSum)
+console.log('test sum:', sum(10, 15))
