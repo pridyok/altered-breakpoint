7cde664ad616d0e09722973ba402f001b6f4d1f9
PridyOK <aaron@alteredpixels.io>
2021-04-29 21:08:35 -0700

    Breakpoint JS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,91 @@
-if (module.hot) module.hot.accept()
+import { debounce } from 'debounce'
 
-export const sum = (a: number, b: number): number => {
-  return a + b
+export class Breakpoint {
+  private static instance: Breakpoint
+  private rootStyles: CSSStyleDeclaration
+  private data: { breakpoint: string }
+  private breakpoints: Record<string, number>
+  private eventDelay: number
+
+  constructor({ breakpoints, delay = 0 }: { breakpoints?: Array<string>|null, delay?: number } = {}) {
+    if (Breakpoint.instance) return Breakpoint.instance
+
+    this.rootStyles = window.getComputedStyle(document.documentElement)
+    this.data = { breakpoint: this.current() }
+    this.breakpoints = {}
+    this.eventDelay = delay
+
+    const breakpointKeyString = this.rootStyles.getPropertyValue('--breakpoint-keys')
+
+    breakpoints ??= breakpointKeyString ? breakpointKeyString.trim().split(' ') : [ 'base', 'xs', 's', 'm', 'l', 'xl' ]
+    this.breakpoints = breakpoints.reduce((result, key, index) => ({ ...result, [key]: index}), {})
+
+    this.bindEvents()
+
+    Breakpoint.instance = this
+    Object.freeze(Breakpoint.instance)
+  }
+
+  bindEvents(): void {
+    window.addEventListener('load', this.update.bind(this))
+    window.addEventListener('resize', (this.eventDelay ? debounce(this.update, this.eventDelay) : this.update).bind(this))
+  }
+
+  weight(value = this.data.breakpoint): number {
+    return this.breakpoints[value]
+  }
+
+  is(target: string, current = this.data.breakpoint): boolean {
+    if (current === target) {
+      return true
+    }
+
+    return false
+  }
+
+  min(target: string, current = this.data.breakpoint): boolean {
+    if (this.weight(current) >= this.weight(target)) {
+      return true
+    }
+
+    return false
+  }
+
+  max(target: string, current = this.data.breakpoint): boolean {
+    if (this.weight(current) <= this.weight(target)) {
+      return true
+    }
+
+    return false
+  }
+
+  emitEvent(newBreakpoint: string): void {
+    const breakpointChange = new CustomEvent('breakpoint', {
+      detail: {
+        bp: {
+          old: this.data.breakpoint,
+          new: newBreakpoint,
+        }
+      }
+    })
+
+    window.dispatchEvent(breakpointChange)
+  }
+
+  current(): string {
+    return this.rootStyles.getPropertyValue('--breakpoint').trim().replace(/^"(.*)"$/, '$1')
+  }
+
+  update(): void {
+    const breakpoint = this.current()
+
+    if (this.data.breakpoint !== breakpoint) {
+      this.emitEvent(breakpoint)
+      this.data.breakpoint = breakpoint
+    }
+  }
 }
 
-console.log('test sum:', sum(10, 15))
+export default Breakpoint
+
+if (module.hot) module.hot.accept()
